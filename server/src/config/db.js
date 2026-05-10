3abf8b6439bc0fe06c5fcbc2d52cbe9a71746fff
Chetan Chauhan <devilmafia71@gmail.com>
2026-05-10 15:48:16 +0530

    some required changes in vite.config

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI is missing. Create server/.env from server/.env.example before starting the server.');
+    }
+
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
     });
